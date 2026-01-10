1d7e792a88d7388adfc823faa41a9cc625d9ede0
Rafail chatzis <xatzisrafail123@gmail.com>
2026-01-11 00:54:53 +0200

    Delete escaperoom directory - i refactored home.html

--- escaperoom/details/booking/logged_in.php
+++
@@ -1,8 +0,0 @@
-<tr>
-    <td> <?= $row->id ?> </td>
-    <td> <?= $row->firstname ?> </td>
-    <td> <?= $row->lastname ?> </td>
-    <td> <?= $row->username ?> </td>
-    <td> <?= $row->gender ?> </td>
-    <td> <?= $row->city ?> </td>
-</tr>
